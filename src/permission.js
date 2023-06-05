fd6561c20cb3b2cee41308b642d5e6d0524c144c
ronlu77 <ron000816@gmail.com>
2023-06-05 20:27:27 +0800

    feat：登录逻辑
    使用全局前置路由守卫和后置路由守卫实现登录

--- src/permission.js
+++ src/permission.js
@@ -13,24 +13,26 @@ const whiteSite = ['/login']
 
 // 全局前置路由守卫, 进行页面访问权限的判断, tip:每次路由跳转时都会执行
 router.beforeEach(async (to, from, next) => {
+    // next 方法带不同参数时,相当于重载，只有next()是直接放行, next({path: '/'}) 会再次被路由守卫拦截
     // 页面加载进度条
     NProgress.start()
     if (getToken()) {
         // 存在 token
         if (to.path === '/login') {
-            //  token 存在说明已登陆不能去登陆界面重定向跳转到首页
+            //  终止路由，再次进入全局前置路由守卫，而不是直接跳转到首页
+            console.log('from', from)
             next({ path: '/' })
             NProgress.done()
         } else {
-            // token 存在跳转到非login路由，先判断用户信息是否存在，存在直接跳转，不存在先拉取用户信息
+            //! token 存在跳转到非login路由，先判断用户信息是否存在，存在直接跳转，不存在先拉取用户信息
             if (store.getters.name) {
                 // 用户信息存在
-                //* 可拓展：根据用户权限获取动态访问的路由信息
                 next()
             } else {
                 // 用户信息不存在通过 token 拉取用户信息
                 try {
                     await store.dispatch('user/GetInfo')
+                    //* 可拓展：根据用户权限获取动态访问的路由信息                    
                     next()
                 } catch (error) {
                     await store.dispatch('user/Logout')
@@ -41,7 +43,6 @@ router.beforeEach(async (to, from, next) => {
             }
         }
     } else {
-        console.log('no token ')
         // 不存在 token
         // 是白名单里面的路由则直接跳转
         if (whiteSite.indexOf(to.path) !== -1) {
